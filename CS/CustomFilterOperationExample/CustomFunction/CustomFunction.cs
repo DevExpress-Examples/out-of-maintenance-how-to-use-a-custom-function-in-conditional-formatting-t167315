850e156ac97c931bb3cae6013d679ff01375941a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomFilterOperationExample/CustomFunction/CustomFunction.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.Data.Filtering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFilterOperationExample
+{
+    public class CustomFunction : ICustomFunctionOperator
+    {
+        public object Evaluate(params object[] operands)
+        {
+            var result = Convert.ToInt32(operands[0]) / Convert.ToInt32(operands[1]);
+
+            if (result == Convert.ToInt32(operands[2]))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Name
+        {
+            get { return "Function"; }
+        }
+
+        public Type ResultType(params Type[] operands)
+        {
+            return typeof(bool);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use a custom function in Conditional Formatting
+
+
+This example demonstrates how to create and use a custom function operator in Conditional Formatting.<br />With custom functions, you can format rows according to custom rules.<br />For this, we implement the ICustomFunctionOperator interface with a custom function. Then, we register this function operator in the CriteriaOperator's RegisterCustomFunction method.
+
+<br/>
+
+
